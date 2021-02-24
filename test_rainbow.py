b82ee1725acfafde42129da684a4472b46345858
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2021-02-24 12:56:59 +0000

    add second Pling test using negative number

--- test_rainbow.py
+++ test_rainbow.py
@@ -7,8 +7,8 @@ class TestRainbow(unittest.TestCase):
 
     # test to see if 12 returns it's correct value, "Pling"
     def test_pling(self):
-        result = raindrop.raindrop(12)
-        self.assertEqual(result, "Pling")
+        self.assertEqual(raindrop.raindrop(12), "Pling")
+        self.assertEqual(raindrop.raindrop(-3), "Pling")
 
 
 # allows the tests to be run in terminal without having to call upon the unittest module
