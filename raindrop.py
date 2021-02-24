8069768010b58ac2870c664c054bac789f50f7db
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2021-02-24 10:58:27 +0000

    pling added

---
+++ raindrop.py
@@ -0,0 +1,12 @@
+
+
+# creating the function
+
+def raindrop(number):
+
+    # checking to see if the number is divisible by 3
+    if number % 3 == 0:
+        # if the number is divisible by 3, 'pling' will be returned
+        return ('pling')
+
+
